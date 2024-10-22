a39855dc5722068cd95c61d0bbd9cc796351bd02
krekosiewicz <rekosiewicz.kacper@gmail.com>
2024-10-22 15:09:07 +0200

    react 19, next 15 playground, use cache directive

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    dynamicIO: true,
+    ppr: true,
+    cacheLife: {
+      frequent: {
+        revalidate: 20,
+        expire: 60,
+      },
+      default: {}
+    }
+  }
 };
 
 export default nextConfig;
